d355217ef362d2c84e556a4e0a8b8aaf857b659c
Johannes Brix <johannes.brix7@gmail.com>
2025-05-18 09:10:12 +0200

    Added the get my latest blog functionality

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+function goToLatestBlog() {
+    const firstBlogLink = document.querySelector('.blog-btn .link')
+    if (firstBlogLink) {
+        window.location.href = firstBlogLink.href
+    }
+}
